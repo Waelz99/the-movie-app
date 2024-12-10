ccb98bec71fc7f6ec6207557bbe31fbf19f8cf66
Waelz99 <waela.ashour1@gmail.com>
2024-12-10 14:20:56 +0200

    Setting up Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set up Swagger
+  const config = new DocumentBuilder()
+    .setTitle('The movie app')
+    .setDescription(
+      'CRUD (Create, Read, Update, Delete) application that consumes the TMDB APIs.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
