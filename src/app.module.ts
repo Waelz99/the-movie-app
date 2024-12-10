69ecd9b7f476416298d511ffe5832180cc4ee47f
Waelz99 <waela.ashour1@gmail.com>
2024-12-10 22:11:22 +0200

    Integrated movies module with AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeOrmConfig from './config/database.config';
+import { MoviesService } from './movies/movies.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MoviesController],
+  providers: [AppService, MoviesService],
 })
 export class AppModule {}
