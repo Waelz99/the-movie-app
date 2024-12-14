6324ced35663a84ae9627e66251480f09c5e635d
Waelz99 <waela.ashour1@gmail.com>
2024-12-14 19:56:42 +0200

    Created TMDBModules

---
+++ src/movies/tmdb/tmdb.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TmdbService } from './tmdb.service';
+import { ConfigModule } from '@nestjs/config';
+import { TMDBHelpers } from '../helpers/tmdb-helpers';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [TmdbService, TMDBHelpers],
+})
+export class TmdbModule {}
