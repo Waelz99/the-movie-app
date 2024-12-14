ed6dbdf6480b5beacd97e3b780c5e704ea22f4d9
Waelz99 <waela.ashour1@gmail.com>
2024-12-14 19:59:15 +0200

    Merged Pull Request: Setting up TMDBService (#12)
    * Added required packages for TMDBServices
    
    * Created TMDBHelpers
    
    * Setting up TMDBServices
    
    * Created TMDBModules
    
    * Added nullable properties to Movies entity
    
    * Removed invalid tests
    
    * Included TMDBModule to MoviesModule

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -4,13 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import moviesOrmConfig from './config/movies.db.config';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
+import { TmdbModule } from './tmdb/tmdb.module';
+import { TmdbService } from './tmdb/tmdb.service';
+import { TMDBHelpers } from './helpers/tmdb-helpers';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(moviesOrmConfig),
     TypeOrmModule.forFeature([Movie]),
+    TmdbModule,
+    ScheduleModule.forRoot(),
   ],
   controllers: [MoviesController],
-  providers: [MoviesService],
+  providers: [MoviesService, TmdbService, TMDBHelpers],
 })
 export class MoviesModule {}
